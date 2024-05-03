d05cfc29de04dd28c990963f274ce25f79880559
qpmpl <58429446+qpmpl@users.noreply.github.com>
2024-05-03 19:08:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 echo "src-git helloworld  https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-# echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git" >> "feeds.conf.default"
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
 echo "src-git small https://github.com/kenzok8/small" >> "feeds.conf.default"
 echo "src-git OpenClash https://github.com/vernesong/OpenClash.git" >> "feeds.conf.default"
